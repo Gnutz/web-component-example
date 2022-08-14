4954b9e3ce4cee91caf21134d32335b960375fe5
Gnutz <kga@live.dk>
2022-08-15 01:46:24 +0200

    5th

--- character-card.js
+++ character-card.js
@@ -35,14 +35,14 @@ class CharacterCard extends HTMLElement {
         /* id = this.getAttribute("charId");
         console.log("id") */
 
-        let name = this.getAttribute("name");
+        let name = this.getAttribute("id");
 
         this.innerHTML =`<article class="characterCard__Wrapper-sc-1ejywvi-0 bEklxv">
         <div class="characterCard__ImgWrapper-sc-1ejywvi-1 fkUcVI"><img src="https://rickandmortyapi.com/api/character/avatar/291.jpeg" alt="${name}"></div>
         <div class="characterCard__ContentWrapper-sc-1ejywvi-2 isMAic">
            <div class="section">
               <a href="https://rickandmortyapi.com/api/character/291" rel="nofollow noopener noreferrer" target="_blank" class="externalLink__ExternalLink-sc-1lixk38-0 ffGNdR">
-                 <h2>${name}<"/h2>
+                 <h2>${name}</h2>
               </a>
               <span class="status"><span class="status__icon"></span> Alive - Human</span>
            </div>
