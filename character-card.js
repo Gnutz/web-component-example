1533f0c2aeffe0d7e9e9efd951ba801ed8b6e3a6
Gnutz <kga@live.dk>
2022-08-15 02:03:33 +0200

    hello

--- character-card.js
+++ character-card.js
@@ -7,44 +7,61 @@ class CharacterCard extends HTMLElement {
     static NUMBEROFCHARACTERS = 826 */
 
     
-/* 
     getRandomId() {
         return Math.floor(Math.random() * NUMBEROFCHARACTERS + 1); 
     }
 
     static get observedAttributes(){
-        return ['charId'];
+        return ['id'];
     }
 
     attributeChangedCallback(name, oldVal, newVal){
-        if (name === 'charId'){
-            this.fetchCharacterById(newVal);
+        if (name === 'id'){
+          
         }
     }
 
     set id(value){
-        this.charId = value < NUMBEROFCHARACTERS ? value : NUMBEROFCHARACTERS;
+        this.id = value < NUMBEROFCHARACTERS ? value : NUMBEROFCHARACTERS;
     }
 
-    set character(character){
-        this.character
-    } */
-
+    
     connectedCallback(){
-
-        /* id = this.getAttribute("charId");
-        console.log("id") */
 
         let name = this.getAttribute("id");
 
+        let character=  JSON.parse(`{
+            "id": 25,
+            "name": "Armothy",
+            "status": "Dead",
+            "species": "unknown",
+            "type": "Self-aware arm",
+            "gender": "Male",
+            "origin": {
+            "name": "Post-Apocalyptic Earth",
+            "url": "https://rickandmortyapi.com/api/location/8"
+            },
+            "location": {
+            "name": "Post-Apocalyptic Earth",
+            "url": "https://rickandmortyapi.com/api/location/8"
+            },
+            "image": "https://rickandmortyapi.com/api/character/avatar/25.jpeg",
+            "episode": [
+            "https://rickandmortyapi.com/api/episode/23"
+            ],
+            "url": "https://rickandmortyapi.com/api/character/25",
+            "created": "2017-11-05T08:54:29.343Z"
+            }`)
+
+
         this.innerHTML =`<article class="characterCard__Wrapper-sc-1ejywvi-0 bEklxv">
-        <div class="characterCard__ImgWrapper-sc-1ejywvi-1 fkUcVI"><img src="https://rickandmortyapi.com/api/character/avatar/291.jpeg" alt="${name}"></div>
+        <div class="characterCard__ImgWrapper-sc-1ejywvi-1 fkUcVI"><img src="https://rickandmortyapi.com/api/character/avatar/291.jpeg" alt="${character["name"]}"></div>
         <div class="characterCard__ContentWrapper-sc-1ejywvi-2 isMAic">
            <div class="section">
               <a href="https://rickandmortyapi.com/api/character/291" rel="nofollow noopener noreferrer" target="_blank" class="externalLink__ExternalLink-sc-1lixk38-0 ffGNdR">
-                 <h2>${name}</h2>
+                 <h2>${character["name"]}</h2>
               </a>
-              <span class="status"><span class="status__icon"></span> Alive - Human</span>
+              <span class="status"><span class="status__icon"></span> ${character["status"]} - Human</span>
            </div>
            <div class="section"><span class="text-gray">Last known location:</span><a href="https://rickandmortyapi.com/api/location/3" rel="nofollow noopener noreferrer" target="_blank" class="externalLink__ExternalLink-sc-1lixk38-0 ffGNdR">Citadel of Ricks</a></div>
            <div class="section"><span class="text-gray">First seen in:</span><a href="https://rickandmortyapi.com/api/episode/28" rel="nofollow noopener noreferrer" target="_blank" class="externalLink__ExternalLink-sc-1lixk38-0 ffGNdR">The Ricklantis Mixup</a></div>
@@ -52,14 +69,8 @@ class CharacterCard extends HTMLElement {
      </article>`
 
 
-/*      this.addEventListener('click', event =>
-            this.fecthRandomCharacter(event)); */
-    
     }
 
-   /*  fecthRandomCharacter(){
-        this.id = this.getRandomId();
-    } */
 
 }
 if (!customElements.get('character-card')) {
